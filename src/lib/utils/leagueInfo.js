d23c58dc80f554a9ad9b530a6c2c139f900d9752
joneskyle0 <93267704+joneskyle0@users.noreply.github.com>
2024-08-17 13:41:45 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -97,7 +97,7 @@ export const managers = [
       "name": "walkerba",
       "location": "Intramural Sports Building 606 E Hoover Ave Ann Arbor, MI", // (optional)
       "bio": "I often do things I’m not good at like play golf and fantasy football but I always keep the vibes high. I am a 7 time IM champion and technically a former D1 athlete.",
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/walker.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2010, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "det", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
