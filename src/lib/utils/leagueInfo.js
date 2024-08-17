78df6e841ec788940dc2a1cb93605272010b6063
joneskyle0 <93267704+joneskyle0@users.noreply.github.com>
2024-08-17 13:18:08 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -229,7 +229,7 @@ export const managers = [
       "name": "jmklein14",
       "location": "Chicago", // (optional)
       "bio": "Football is fun and so is having JJets 🤠",
-      "photo": "/managers/jklein.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/jklein.jpeg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2017, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "det", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
