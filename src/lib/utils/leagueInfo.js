d7b43cee4d6a2b70bd1f34e3d9ac67ff8146eed6
joneskyle0 <93267704+joneskyle0@users.noreply.github.com>
2024-08-17 13:06:28 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -185,7 +185,7 @@ export const managers = [
       "name": "brendon13bouch",
       "location": "Ireland", // (optional)
       "bio": "Dan Campbells son",
-      "photo": "/managers/bbouch.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/bbouc.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "det", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
