8333f5614c5c90a4861fc5deee80d91df8462938
joneskyle0 <93267704+joneskyle0@users.noreply.github.com>
2024-08-18 00:05:04 -0400

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -289,10 +289,10 @@
     <h3 bind:this={sevenTwo}>7.2 Payout</h3>
     <p>League payout is structured as follows:</p>
     <ul>
-        <li>1st place: {dues * 6}$</li>
-        <li>2nd place: {dues * 2}$</li>
-        <li>3rd place: {dues}$</li>
-        <li>Highest scoring team during the regular season: {dues}</li>
+        <li>1st place: ${dues * 6}</li>
+        <li>2nd place: ${dues * 2}</li>
+        <li>3rd place: ${dues}</li>
+        <li>Highest scoring team during the regular season: ${dues}</li>
     </ul>
     <p>In the event that a manager was fined due to late payment, their extra fee will be added to the winner's payout. Similarly, if new managers come into the league and the payout is smaller, the difference will come out of the winner's prize.</p>
     
